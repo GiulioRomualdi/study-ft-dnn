d26f9564d9479ad2b7c8fdaa4af7906002e7d367
Giulio Romualdi <giulio.romualdi@gmail.com>
2024-02-01 18:10:52 +0100

    Update the dataset and the train script for sklearn

--- ft_denoising/dataset.py
+++ ft_denoising/dataset.py
@@ -5,62 +5,82 @@ from pathlib import Path
 import h5py
 from typing import List
 
-
 class FTDataset(Dataset):
     def __init__(
         self,
-        dataset_path_folder: Path,
+        datasets_path_folder: Path,
         ft_name: str,
         device=torch.device("cpu"),
-        max_raw_ft: float = None,
-        max_expected: float = None,
-        max_temperature: float = None,
-    ):
-      
+        max_raw_ft: torch.Tensor = None,
+        max_temperature: torch.Tensor = None,
+        max_accelerometer: torch.Tensor = None,
+        max_gyro: torch.Tensor = None,
+        max_expected: torch.Tensor = None,
+    ):      
         self.device = device
 
-        # open the file with h5py
-        print("Opening file: ", dataset_path_folder)
-        with h5py.File(str(dataset_path_folder), "r") as file:
+        # get all the mat files in the folder
+        self.datasets_path_folder = datasets_path_folder
+        self.datasets_path_folder = self.datasets_path_folder.glob("*.mat")
+        self.datasets_path_folder = list(self.datasets_path_folder)
 
-            # read the data
-            self.raw_ft = file[ft_name]['data'][:].T
-            self.raw_ft = torch.tensor(self.raw_ft, dtype=torch.float32, device=self.device)
+        self.raw_ft = torch.empty(0, 6, device=self.device)
+        self.temperature = torch.empty(0, 1, device=self.device)
+        self.accelerometer = torch.empty(0, 3, device=self.device)
+        self.gyro = torch.empty(0, 3, device=self.device)
+        self.expected = torch.empty(0, 6, device=self.device)
 
-            # normalize the data by getting the max value in each column
-            if max_raw_ft is None:
-                self.max_raw_ft = torch.max(torch.abs(self.raw_ft), dim=0).values
-            else:
-                self.max_raw_ft = max_raw_ft   
-            
-            self.raw_ft = self.raw_ft / self.max_raw_ft
+        for dataset_path_folder in self.datasets_path_folder:
+            raw_ft, gyro, accelerometer, temperature, expected = self._load_one_dataset(dataset_path_folder, ft_name)
 
-            
-            self.expected = file[ft_name]['expected'][:].T
-            self.expected = torch.tensor(self.expected, dtype=torch.float32, device=self.device)
+            self.raw_ft = torch.cat((self.raw_ft, raw_ft), 0)
+            self.temperature = torch.cat((self.temperature, temperature), 0)
+            self.accelerometer = torch.cat((self.accelerometer, accelerometer), 0)
+            self.gyro = torch.cat((self.gyro, gyro), 0)
+            self.expected = torch.cat((self.expected, expected), 0)
 
-            # normalize the data by getting the max value in each column
-            if max_expected is None:
-                self.max_expected = torch.max(torch.abs(self.expected), dim=0).values
-            else:
-                self.max_expected = max_expected
+        # normalize the data
+        if max_raw_ft is None:
+            self.max_raw_ft = torch.max(torch.abs(self.raw_ft), dim=0).values
+        else:
+            self.max_raw_ft = max_raw_ft
+        self.raw_ft = self.raw_ft / self.max_raw_ft
 
-            self.expected = self.expected / self.max_expected
+        if max_temperature is None:
+            self.max_temperature = torch.max(torch.abs(self.temperature), dim=0).values
+        else:
+            self.max_temperature = max_temperature
+        self.temperature = self.temperature / self.max_temperature
 
-            self.temperature = file[ft_name]['temperature'][:].T
-            self.temperature = torch.tensor(self.temperature, dtype=torch.float32, device=self.device)
+        if max_accelerometer is None:
+            self.max_accelerometer = torch.max(torch.abs(self.accelerometer), dim=0).values
+        else:
+            self.max_accelerometer = max_accelerometer
 
-            # normalize the data
-            if max_temperature is None:
-                self.max_temperature = torch.max(torch.abs(self.temperature))
-            else:
-                self.max_temperature = max_temperature
+        self.accelerometer = self.accelerometer / self.max_accelerometer
 
-            self.temperature = self.temperature / self.max_temperature
+        if max_gyro is None:
+            self.max_gyro = torch.max(torch.abs(self.gyro), dim=0).values
+        else:
+            self.max_gyro = max_gyro
+
+        self.gyro = self.gyro / self.max_gyro
+
+        if max_expected is None:
+            self.max_expected = torch.max(torch.abs(self.expected), dim=0).values
+        else:
+            self.max_expected = max_expected
+
+        self.expected = self.expected / self.max_expected
+
+        print(self.expected.shape)
+
 
     def __getitem__(self, index):
         return {
             "ft_raw": self.raw_ft[index,:],
+            "gyro": self.gyro[index,:],
+            "accelerometer": self.accelerometer[index,:],
             "temperature": self.temperature[index,:],
         }, {
             "expected": self.expected[index,:],
@@ -69,10 +89,35 @@ class FTDataset(Dataset):
     def __len__(self):
         return self.raw_ft.shape[0]
 
+    def _load_one_dataset(self, dataset_path_folder, ft_name):
+        # open the file with h5py
+        print("Opening file: ", dataset_path_folder)
+        with h5py.File(str(dataset_path_folder), "r") as file:
+            file = file["parsedFile"]
+            file_measures = file["measures"]
+
+            # read the data
+            raw_ft = file_measures["FTs"][ft_name]['data'][:].T
+            raw_ft = torch.tensor(raw_ft, dtype=torch.float32, device=self.device)
+            
+            gyro = file_measures["gyros"][ft_name]['data'][:].T
+            gyro = torch.tensor(gyro, dtype=torch.float32, device=self.device)
+
+            accelerometer = file_measures["accelerometers"][ft_name]['data'][:].T
+            accelerometer = torch.tensor(accelerometer, dtype=torch.float32, device=self.device)
+
+            temperature = file_measures["temp"][ft_name]['data'][:].T
+            temperature = torch.tensor(temperature, dtype=torch.float32, device=self.device)
+
+            expected = file["expectedValues"]["FTs"][ft_name]['data'][:].T
+            expected = torch.tensor(expected, dtype=torch.float32, device=self.device)
+
+            return raw_ft, gyro, accelerometer, temperature, expected
+
 
 if __name__ == "__main__":
     training_data = FTDataset(
-        dataset_path_folder=Path("/home/gromualdi/robot-code/test-ft-dnn/datasets/r_leg_ft_calib_nn.mat"),
+        datasets_path_folder=Path("/home/gromualdi/robot-code/element_ft-nonlinear-modeling/code/matlab/src/training"),
         ft_name="r_leg_ft",
     )
     print(training_data[0])
@@ -80,7 +125,7 @@ if __name__ == "__main__":
     from torch.utils.data import DataLoader
 
     train_dataloader = DataLoader(training_data, batch_size=5, shuffle=False)
-    rain_features, train_labels = next(iter(train_dataloader))
+    train_features, train_labels = next(iter(train_dataloader))
     # print(
     #     f"Feature batch shape: {train_features['data'].size()}"
     # )  # batch_size, sequence_length, 6
---
+++ train_sklearn.py
@@ -0,0 +1,96 @@
+from ft_denoising.dataset import FTDataset
+import joblib
+
+
+from pathlib import Path
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.kernel_ridge import KernelRidge
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+
+# Create the dataset
+def get_dataset(device="cpu"):
+    training_data = FTDataset(
+            datasets_path_folder=Path("/home/gromualdi/robot-code/element_ft-nonlinear-modeling/code/matlab/src/training"),
+            ft_name="r_leg_ft",
+            device=device,
+        )
+
+    # validation_data = FTDataset(
+    #         dataset_path_folder=Path("/home/gromualdi/robot-code/test-ft-dnn/datasets/r_leg_ft_valid_nn.mat"),
+    #         ft_name="r_leg_ft",
+    #         device=device,
+    #         max_accelerometer=training_data.max_accelerometer,
+    #         max_gyro=training_data.max_gyro,
+    #         max_raw_ft=training_data.max_raw_ft,
+    #         max_temperature=training_data.max_temperature,
+    #         max_expected=training_data.max_expected,
+    #     )
+
+    # load dataset in numpy
+    training_data_raw_ft = training_data.raw_ft.cpu().numpy()
+    training_data_temperature = training_data.temperature.cpu().numpy()
+    training_data_accelerometer = training_data.accelerometer.cpu().numpy()
+    training_data_gyro = training_data.gyro.cpu().numpy()
+    training_data_expected = training_data.expected.cpu().numpy()
+
+    # stack the data together
+    X_training = np.hstack((training_data_raw_ft, training_data_accelerometer, training_data_gyro, training_data_temperature))
+    y_training = training_data_expected
+
+    # shuffle the data
+    idx = np.random.permutation(len(X_training))
+    X_training = X_training[idx]
+    y_training = y_training[idx]
+
+    """ validation_data_raw_ft = validation_data.raw_ft.cpu().numpy()
+    validation_data_temperature = validation_data.temperature.cpu().numpy()
+    validation_data_expected = validation_data.expected.cpu().numpy()
+
+    # stack the data together
+    X_validation = np.hstack((validation_data_raw_ft, validation_data_temperature))
+    y_validation = validation_data_expected
+    """
+
+    return X_training, y_training,
+
+def train():
+    X_train, Y_train = get_dataset()
+
+    # perform crossvalidation of the hyperparameters using `rbf` kernel
+    param_grid = {
+        'alpha': [1e0, 0.1, 1e-2, 1e-3],
+        'gamma': np.logspace(-2, 2, 5),
+        'kernel': ['rbf'],
+    }
+
+    # Create a base model
+    kr = KernelRidge()
+
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(kr, param_grid, cv=5, scoring='neg_mean_squared_error', verbose=2, n_jobs=5)
+
+    # Fit the model using the training data
+    grid_search.fit(X_train, Y_train)
+
+    # Get the best hyperparameters
+    best_params = grid_search.best_params_
+
+    # Print the best hyperparameters
+    print("Best Hyperparameters:", best_params)
+
+    # # Use the best model to make predictions on the validation set
+    # Y_pred = grid_search.best_estimator_.predict(X_val)
+
+    # # Evaluate the model on the validation set
+    # mse = mean_squared_error(Y_val, Y_pred)
+    # print("Mean Squared Error on Test Set:", mse)
+
+
+    # save the model
+    joblib.dump(grid_search.best_estimator_, 'model.pkl')
+
+if __name__ == "__main__":
+
+    train()
